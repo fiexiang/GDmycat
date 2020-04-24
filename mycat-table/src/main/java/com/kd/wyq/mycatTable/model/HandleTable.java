e03c7f032f6aa91e1b035780a31660d9006488cf
wangyuqiang <19960408q>
2020-04-24 11:19:20 +0800

    添加了操作表相关功能

---
+++ mycat-table/src/main/java/com/kd/wyq/mycatTable/model/HandleTable.java
@@ -0,0 +1,23 @@
+package com.kd.wyq.mycatTable.model;
+
+public class HandleTable {
+
+    private Table table;
+    private String handle;
+
+    public Table getTable() {
+        return table;
+    }
+
+    public void setTable(Table table) {
+        this.table = table;
+    }
+
+    public String getHandle() {
+        return handle;
+    }
+
+    public void setHandle(String handle) {
+        this.handle = handle;
+    }
+}
